d66714169927705dcb5a90173f68ded63fe17a34
Carolina Pereira <inaciopereira.carolina@gmail.com>
2020-11-10 10:30:12 +0100

    added api functions for article and catgory

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Article } from './article';
 import { Observable } from 'rxjs';
+import { Category } from './category';
+import { Article } from './article';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  PHP_API_SERVER = 'http://m533-backend.test';
+  PHP_API_SERVER_V1 = 'http://m533-backend.test/api/v1';
 
   constructor(private httpClient: HttpClient) {
   }
 
   getAllArticles(): Observable<Article[]> {
-    return this.httpClient.get<Article[]>(`${this.PHP_API_SERVER}/api/v1/articles`);
+    return this.httpClient.get<Article[]>(`${this.PHP_API_SERVER_V1}/articles`);
+  }
+
+  getArticlesByCategory(categoryId: number): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(`${this.PHP_API_SERVER_V1}/articles?category=${categoryId}`);
+  }
+
+  getArticleById(id: number): Observable<Article> {
+    return this.httpClient.get<Article>(`${this.PHP_API_SERVER_V1}/articles/${id}`);
+  }
+
+  getCategories(): Observable<Category[]>{
+    return this.httpClient.get<Category[]>(`${this.PHP_API_SERVER_V1}/categories`);
+  }
+
+  getCategory(id: number): Observable<Category> {
+    return this.httpClient.get<Category>(`${this.PHP_API_SERVER_V1}/categories/${id}`);
   }
 }

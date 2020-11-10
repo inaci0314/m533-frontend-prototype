b2e8042c6839656cb87113c2792e33c9227183c5
Carolina Pereira <inaciopereira.carolina@gmail.com>
2020-11-10 10:32:29 +0100

    removed articles file, updated articles components

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { articles } from "../articles";
+
+import { ApiService } from '../api.service';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article',
@@ -8,13 +10,16 @@ import { articles } from "../articles";
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
-  article;
+  article: Article;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.article = articles[+params.get("id")];
+      this.apiService.getArticleById(+params.get("id")).subscribe((article: Article) => {
+        this.article = article;
+        // TODO: return error 404 (error page) if item doesn't exist
+      })
     })
   }
 
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { ApiService } from '../api.service';
 import { Article } from '../article';
+import { Category } from '../category';
 
 @Component({
   selector: 'app-articles',
@@ -8,15 +11,36 @@ import { Article } from '../article';
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
+  categoryId: number;
+  category: Category;
   articles: Article[];
-  constructor(private apiService: ApiService) { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
+  }
 
   ngOnInit(): void {
-    this.apiService.getAllArticles().subscribe((articles: Article[]) => {
-      this.articles = articles;
-      console.log(this.articles);
+    this.route.queryParams.subscribe(params => {
+      this.categoryId = params['category'];
+      console.log(this.categoryId);
+    })
+
+    // If there's a category id, get articles by category
+    if (this.categoryId != null) {
+      this.apiService.getArticlesByCategory(this.categoryId).subscribe((articles: Article[]) => {
+        this.articles = articles;
+        // And get the category
+        this.apiService.getCategory(this.categoryId).subscribe((category: Category) => {
+          this.category = category;
+          console.log(this.category);
+        })
+      })
     }
-    )
+    else {
+      // Otherwise, get all articles
+      this.apiService.getAllArticles().subscribe((articles: Article[]) => {
+        this.articles = articles;
+        console.log(this.articles);
+      })
+    }
   }
 
 }

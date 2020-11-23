454fd5f3eebfaa7a7578cc6af63f07096e000e81
Carolina Pereira <inaciopereira.carolina@gmail.com>
2020-11-23 20:41:40 +0100

    Fixed issue with navbar after adding ReactiveForms

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
+  myForm:FormGroup;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ ngOnInit(){
+         this.myForm = new FormGroup({
+           
+         })
+    }
 
 }
